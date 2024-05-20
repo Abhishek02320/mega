01b9adbf86df6a10693c0cabee0a96f885fb75d1
Abhishek02320 <113132228+Abhishek02320@users.noreply.github.com>
2024-05-20 19:37:32 +0530

    hello

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+
+
+
+
+// jaisa ki hame pata h ki jab bhi ham apne server ki file me ya poore backend ke code me khuch bhi naya code edit karte h ya kuch edit karte h to hame server ko baar baar (stop karke start) karna padta h har baar jab bhi ham backend ki files me kuch edit karte h 
+//to ye baar baar hame server (start ya stop) na karna pade code edit ke baad for see your edit code result ham iske liye (nodemon) ka use karenge iska kaam hota h ki jaise hi hamari server ki file me kuch bhi edit ho ye apne aap server ko band karke apne aap dubara start kar de jisse hame ye sab khudse na karna pade 
+// npm i nodemon        // aise karke isko install karenge to ye main dependency ban jaayegi but ham chhhate h ki ye devdependency bane (devdependency)matalab ki jab ham apna project taiyaar kar rahe sirf tabhi tak ye use ho hamare code me aur jaise hi is code ko industry me use kare to waha nodemon use hona band ho jaaye because iska use bhi to hame tabhi tak h jab tak ham is project ko bana rahe h to file edit karte h to apne aap ye (server ko stop than start)kare aur industry jab use hoga tab koi online user is code me edit to karega nahi to waha use karne ka koi fayada nahi h (isse project ki file halka ya bhaari hone ka koi sambandh nahi h koi farak nahi padta h)
+// (npm install --save-dev nodemon) or (npm i -D nodemon)
